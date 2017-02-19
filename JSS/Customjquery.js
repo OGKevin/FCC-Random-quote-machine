0d09673afda298201f1aa440640002b3bff23263
OGKevin <Kevinhellemun@icloud.com>
2017-02-19 15:29:56 +0100

    inital commit

---
+++ JSS/Customjquery.js
@@ -0,0 +1,37 @@
+var jsonQuotes, i = 0
+	, hexColorVar = (function () {
+		var json = null;
+		$.ajax({
+			'async': false
+			, 'global': false
+			, 'url': "./JSON/HEXColors.json"
+			, 'dataType': "json"
+			, 'success': function (data) {
+				json = data;
+			}
+		});
+		return json;
+	})();
+
+function inr() {
+	i = Math.floor((Math.random() * 101) + 1);
+	console.log(i);
+	return i;
+}
+$.getJSON('./JSON/quotes1.json', {
+	param1: 'value1'
+}, function (json, textStatus) {
+	jsonQuotes = json;
+	$("#quoteBtn").click(function (event) {
+		$("#paragraph").html(jsonQuotes[inr()].quote + "<br>" + "-" + jsonQuotes[i].name);
+		$("#twitterBtn").attr('href', "https://twitter.com/intent/tweet?text=" + jsonQuotes[i].quote + " --" + jsonQuotes[i].name);
+		$("#body").css({
+			"background-color": hexColorVar[i].hex
+			, "transition": "background-color 3s ease"
+		})
+		$("#quoteBtn, #tweet").css({
+			"background-color": hexColorVar[i].hex
+			, "transition": "background-color 3s ease"
+		})
+	});
+});
---
+++ README.md
@@ -0,0 +1,8 @@
+##Random Quote Machine 
+A FCC project. (https://www.freecodecamp.com/challenges/build-a-random-quote-machine)
+
+As explained on the FCC webiste the user should be able to do the follwoing:
+*  I can click a button to show me a new random quote.
+* I can press a button to tweet out a quote.
+
+I remade their example.
