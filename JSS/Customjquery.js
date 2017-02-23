86c865c362ad50a5c6b8f51252b2c22f7ce32a8a
OGKevin <Kevinhellemun@icloud.com>
2017-02-23 21:39:53 +0100

    Changed the way JSON loaded

--- JSS/Customjquery.js
+++ JSS/Customjquery.js
@@ -1,37 +1,37 @@
-var jsonQuotes, i = 0
-	, hexColorVar = (function () {
-		var json = null;
-		$.ajax({
-			'async': false
-			, 'global': false
-			, 'url': "./JSON/HEXColors.json"
-			, 'dataType': "json"
-			, 'success': function (data) {
-				json = data;
-			}
-		});
-		return json;
-	})();
+var jsonQuotes, i = 0;
 
 function inr() {
-	i = Math.floor((Math.random() * 101) + 1);
-	console.log(i);
-	return i;
+    i = Math.floor((Math.random() * 101) + 1);
+    return i;
 }
 $.getJSON('./JSON/quotes1.json', {
-	param1: 'value1'
-}, function (json, textStatus) {
-	jsonQuotes = json;
-	$("#quoteBtn").click(function (event) {
-		$("#paragraph").html(jsonQuotes[inr()].quote + "<br>" + "-" + jsonQuotes[i].name);
-		$("#twitterBtn").attr('href', "https://twitter.com/intent/tweet?text=" + jsonQuotes[i].quote + " --" + jsonQuotes[i].name);
-		$("#body").css({
-			"background-color": hexColorVar[i].hex
-			, "transition": "background-color 3s ease"
-		})
-		$("#quoteBtn, #tweet").css({
-			"background-color": hexColorVar[i].hex
-			, "transition": "background-color 3s ease"
-		})
-	});
+    param1: 'value1'
+}, function(json, textStatus) {
+
+
+    jsonQuotes = json;
+    $("#quoteBtn").click(function(event) {
+        $("#paragraph").html(jsonQuotes[inr()].quote + "<br>" + "-" + jsonQuotes[i].name);
+        $("#twitterBtn").attr('href', "https://twitter.com/intent/tweet?text=" + jsonQuotes[i].quote + " --" + jsonQuotes[i].name);
+
+        $.ajax({
+                url: './JSON/HEXColors.json',
+                dataType: 'json',
+            })
+            .done(function(data) {
+                $("#quoteBtn, #tweet").css({
+                    "background-color": data[i].hex,
+                    "transition": "background-color 3s ease"
+                })
+                $("#body").css({
+                    "background-color": data[i].hex,
+                    "transition": "background-color 3s ease"
+                })
+            })
+            .fail(function() {
+            })
+            .always(function() {
+            });
+
+    });
 });
